d1dddad5476bb078cb3e289b5cc87de1e4c4bacc
Patrick Bittner <patrick.bittner@hamburg.de>
2017-12-03 12:41:58 +0100

    dies und das

---
+++ application/src/main/javascript/meld/src/app/social/people/following-view/following-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-following-view',
+  templateUrl: './following-view.component.html',
+  styleUrls: ['./following-view.component.css']
+})
+export class FollowingViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
