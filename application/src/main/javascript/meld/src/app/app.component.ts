83d62546bdf3df2ef92525a4cfab47d808dfac2b
Patrick Bittner <patrick.bittner@hamburg.de>
2018-01-17 21:29:33 +0100

    navigation loading indicator

--- application/src/main/javascript/meld/src/app/app.component.ts
+++ application/src/main/javascript/meld/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, HostListener, ViewEncapsulation} from '@angular/core';
 import {AppService} from "./app.service";
 import {Link} from "../lib/common/rest/Link";
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,26 @@ export class AppComponent {
 
   public links : Link[];
 
-  constructor(service : AppService) {
+  public loading : boolean = false;
+
+  constructor(service : AppService,
+              router : Router) {
+    router.events.subscribe((event: RouterEvent) => {
+      if (event instanceof NavigationStart) {
+        this.loading = true
+      }
+      if (event instanceof NavigationEnd) {
+        this.loading = false
+      }
+      // Set loading state to false in both of the below events to hide the spinner in case a request fails
+      if (event instanceof NavigationCancel) {
+        this.loading = false
+      }
+      if (event instanceof NavigationError) {
+        this.loading = false
+      }
+    });
+
     this.links = service.configuration.links;
     service.event.subscribe((linksContainer) => {
       this.links = linksContainer.links;
