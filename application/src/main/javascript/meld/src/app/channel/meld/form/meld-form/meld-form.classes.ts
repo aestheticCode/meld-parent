f1629e1dcccecc21893610626f415be6d847b944
Patrick Bittner <patrick.bittner@hamburg.de>
2018-03-03 14:30:39 +0100

    bugfix avatar uri

--- application/src/main/javascript/meld/src/app/channel/meld/form/meld-form/meld-form.classes.ts
+++ application/src/main/javascript/meld/src/app/channel/meld/form/meld-form/meld-form.classes.ts
@@ -1,12 +1,13 @@
 import {MeldPost} from './meld-form.interfaces';
 import {Link} from '../../../../../lib/common/rest/Link';
+import {Category} from '../../../../social/people/category/categories.interfaces';
 
 export class MeldFormPostComponent {
   public post: MeldPost;
 }
 
 export abstract class MeldPostModel implements MeldPost {
-  category: string = null;
+  category: Category = null;
   type: string;
   id: string = null;
   text: string = null;
--- application/src/main/javascript/meld/src/app/channel/meld/form/meld-form/meld-form.interfaces.ts
+++ application/src/main/javascript/meld/src/app/channel/meld/form/meld-form/meld-form.interfaces.ts
@@ -1,8 +1,9 @@
 import {LinksContainer} from '../../../../../lib/common/rest/LinksContainer';
+import {Category} from '../../../../social/people/category/categories.interfaces';
 
 export interface MeldPost extends LinksContainer {
   id: string
   text: string
   type: string
-  category : string
+  category : Category
 }
